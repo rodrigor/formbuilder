a4bc098f67d50840e1d694d20d6fb7fd03ffb413
Raimundo Marcos Gonaçalves Ludgerio <raimundo.marcos@dcx.ufpb.br>
2021-06-29 18:54:15 -0300

    implementado a funcionalidade de cadastro de produto, juntamente com o formulario de cadastro

--- formbuilder/src/main/java/br/ufpb/dcx/aps/formbuilder/controllers/ProdutoController.java
+++ formbuilder/src/main/java/br/ufpb/dcx/aps/formbuilder/controllers/ProdutoController.java
@@ -6,32 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/produtos")
+@RequestMapping(value = "/produtos/")
 public class ProdutoController {
 
     @Autowired
     ProdutoService produtoService;
 
-
-    @GetMapping(value = "", produces = "application/json")
-    public ModelAndView formCadastrarProduto(@RequestBody Produto produto) {
-        return new ModelAndView("criarNovoProduto");
-    }
-
-    @PostMapping(value = "/criar", produces = "application/json")
-    public ModelAndView createNewUser(@RequestBody Produto produto) {
+    @GetMapping(value = "/", produces = "text/html")
+    public ModelAndView formCadastrarProduto() {
         ModelAndView mv = new ModelAndView("criarNovoProduto");
-        mv.addObject("produto", new Produto());
-        produtoService.cadastrarNovoProduto(produto);
-        return new ModelAndView("criarNovoProduto");
+        mv.addObject("produtoForm", new Produto());
+        return mv;
     }
 
+    @PostMapping(value = "/criar", produces = "text/html")
+    public ModelAndView createNewUser(@ModelAttribute Produto produto) {
+        produtoService.cadastrarNovoProduto(produto);
+        return new ModelAndView("index");
+    }
 
 }
