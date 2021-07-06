879862d020c85b7f51f4525022318429ce2c388d
JacksonAGomes <jacksonanderson1515@hotmail.com>
2021-07-06 16:13:42 -0300

    implementado o método de criar formulários da classe FormularioController

--- formbuilder/src/main/java/br/ufpb/dcx/aps/formbuilder/controllers/FormularioController.java
+++ formbuilder/src/main/java/br/ufpb/dcx/aps/formbuilder/controllers/FormularioController.java
@@ -1,14 +1,12 @@
 package br.ufpb.dcx.aps.formbuilder.controllers;
 
 
+import br.ufpb.dcx.aps.formbuilder.DTOs.FormularioDTO;
 import br.ufpb.dcx.aps.formbuilder.models.Formulario;
 import br.ufpb.dcx.aps.formbuilder.services.FormularioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -31,7 +29,7 @@ public class FormularioController {
     @GetMapping(value = "/details", produces = "text/html")
     public ModelAndView visualizarFormulario(@RequestParam("titulo") String titulo) {
         ModelAndView mv = new ModelAndView("visualizarFormulario");
-        Formulario formulario = this.formularioService.pegarPorTitulo(titulo);
+        Formulario formulario = this.formularioService.buscaFormulario(titulo);
         mv.addObject("formDinamico", formulario);
         return mv;
     }
@@ -42,5 +40,10 @@ public class FormularioController {
         Formulario formulario = this.formularioService.buscaFormulario(formularioId);
         mv.addObject("formDinamico", formulario);
         return mv;
+    }
+
+    @PostMapping("/criar")
+    public Formulario criarFormulario(FormularioDTO formulario){
+        return this.formularioService.salvarFormulario(formulario);
     }
 }
